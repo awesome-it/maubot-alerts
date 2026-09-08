e9b2860d74c9cc625dbb15aba6e0a15ae8a50c0a
Lars Kaiser <lars.kaiser@awesome-it.de>
2026-09-08 16:33:25 +0200

    refactor(reactions): remove duplicate code and use enum

--- alertbot/reaction.py
+++ alertbot/reaction.py
@@ -1,10 +1,30 @@
 from __future__ import annotations
 
+from enum import StrEnum
+
 from maubot.handlers import event
 from mautrix.errors import MatrixUnknownRequestError, MNotFound
-from mautrix.types import EventType, StateEvent
+from mautrix.types import EventID, EventType, RoomID, StateEvent
 
 import alertbot
+from alertbot.alerts import AlertGroup
+
+
+class ReactionAction(StrEnum):
+    ACKNOWLEDGE = "👍"
+    UNACKNOWLEDGE = "👎"
+    MANUALLY_RESOLVE = "✅"
+    INVALID = ""
+
+    @classmethod
+    def _missing_(cls, value: object) -> ReactionAction:
+        if value in ["👍", "👍️", "👍🏻", "👍🏽", "👍🏾", "👍🏿"]:
+            return ReactionAction.ACKNOWLEDGE
+        if value in ["👎", "👎️", "👎🏻", "👎🏽", "👎🏾", "👎🏿"]:
+            return ReactionAction.UNACKNOWLEDGE
+        if value in ["✅", "✅️"]:
+            return ReactionAction.MANUALLY_RESOLVE
+        return cls.INVALID
 
 
 class AlertBotReactionManager:
@@ -25,43 +45,42 @@ class AlertBotReactionManager:
             self.bot.log.error(f"Error while reacting to message {event_id} in room {room_id}: {e}")
 
     @event.on(EventType.REACTION)
-    async def handle_reaction(self, evt: StateEvent):
+    async def handle_reaction_event(self, evt: StateEvent):
         if evt.sender == self.bot.client.mxid:
             return
         room_id = evt.room_id
+        reaction_action = ReactionAction(evt.content.relates_to.key)
+        if reaction_action == ReactionAction.INVALID:
+            return
         related_event_id = evt.content.relates_to.event_id
-        reaction_key = evt.content.relates_to.key
         alertgroup = await self.bot.db.get_alertgroup_from_event_id(related_event_id)
-        self.bot.log.debug(f"Received reaction {reaction_key} to alert: {alertgroup}")
         if not alertgroup or not alertgroup.id:
             return
+
+        alertgroup.last_actor = evt.sender
         alertgroup.set_alerts(await self.bot.db.get_alerts_in_alertgroup(alertgroup.id))
         for a in alertgroup.firing_alerts + alertgroup.resolved_alerts:
             a.generate_message(self.bot.templates)
-        if alertgroup and reaction_key in ["👍", "👍️", "👍🏻", "👍🏽", "👍🏾", "👍🏿"]:
-            alertgroup.status = "acknowledged"
-            alertgroup.last_actor = evt.sender
-            alertgroup.generate_message(self.bot.templates)
-            await self.bot.messages.edit_message(room_id, related_event_id, html=alertgroup.message)
-            await self.react_to_message(room_id, related_event_id, reaction_key)
-            await self.bot.db.upsert_alertgroup(alertgroup)
-        elif alertgroup and reaction_key in ["✅", "✅️"]:
-            alertgroup.status = "manually resolved"
-            alertgroup.last_actor = evt.sender
-            alertgroup.generate_message(self.bot.templates)
-            await self.bot.messages.edit_message(room_id, related_event_id, html=alertgroup.message)
-            await self.react_to_message(room_id, related_event_id, reaction_key)
-            await self.bot.messages.pin_unpin_messages(room_id, to_unpin=[related_event_id])
-            await self.bot.db.delete_alertgroup(alertgroup)
-        elif (
-            alertgroup
-            and alertgroup.status == "acknowledged"
-            and reaction_key in ["👎", "👎️", "👎🏻", "👎🏽", "👎🏾", "👎🏿"]
-        ):
-            alertgroup.status = "firing"
-            alertgroup.last_actor = evt.sender
-            alertgroup.generate_message(self.bot.templates)
-            await self.bot.messages.edit_message(room_id, related_event_id, html=alertgroup.message)
-            await self.react_to_message(room_id, related_event_id, reaction_key)
-            await self.bot.messages.pin_unpin_messages(room_id, to_pin=[related_event_id])
-            await self.bot.db.upsert_alertgroup(alertgroup)
+
+        await self.handle_reaction(alertgroup, related_event_id, room_id, reaction_action)
+
+    async def handle_reaction(
+        self, alertgroup: AlertGroup, event_id: EventID, room_id: RoomID, action: ReactionAction
+    ) -> None:
+        match action:
+            case ReactionAction.ACKNOWLEDGE:
+                alertgroup.status = "acknowledged"
+                await self.bot.db.upsert_alertgroup(alertgroup)
+                await self.bot.messages.pin_unpin_messages(room_id, to_pin=[event_id])
+            case ReactionAction.UNACKNOWLEDGE:
+                alertgroup.status = "firing"
+                await self.bot.db.upsert_alertgroup(alertgroup)
+                await self.bot.messages.pin_unpin_messages(room_id, to_pin=[event_id])
+            case ReactionAction.MANUALLY_RESOLVE:
+                alertgroup.status = "manually resolved"
+                await self.bot.db.delete_alertgroup(alertgroup)
+                await self.bot.messages.pin_unpin_messages(room_id, to_unpin=[event_id])
+
+        alertgroup.generate_message(self.bot.templates)
+        await self.bot.messages.edit_message(room_id, event_id, html=alertgroup.message)
+        await self.react_to_message(room_id, event_id, action)
